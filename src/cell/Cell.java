2c1d91d3a83c982c5c8bc28eaddba0c320dab133
vinjerdim <marjer17dim@gmail.com>
2017-03-27 16:29:19 +0700

    Gilak

--- src/cell/Cell.java
+++ src/cell/Cell.java
@@ -1,19 +1,41 @@
 /**
- * Cell merupakan abstract class untuk semua jenis cell
+ * Cell merupakan superclass untuk semua jenis cell dalam Zoo
  */
 package cell;
 import util.Point;
 /**
- * @author vinjerdim
+ * @author Marvin Jerremy Budiman (13515076)
  *
  */
-public class Cell implements Renderable {
+public class Cell {
   private Point coordinate;
-  /* (non-Javadoc)
-   * @see cell.Renderable#render()
+  
+  /**
+   * Konstruktor kelas Cell
+   * I.S. sembarang
+   * F.S. coordinate tercipta
+   * @param absis absis dari cell
+   * @param ordinat ordinat dari cell
    */
-  @Override
-  public void render() {
-	System.out.println("");	
+  public Cell(int absis,int ordinat) {
+	coordinate = new Point(absis,ordinat);
+  }
+  
+  /**
+   * I.S. sembarang
+   * F.S. sama dengan I.S.
+   * @return absis dari cell
+   */
+  public int getCellAbsis() {
+	return coordinate.getAbsis();
+  }
+  
+  /**
+   * I.S. sembarang
+   * F.S. sama dengan I.S.
+   * @return ordinat dari cell
+   */
+  public int getCellOrdinat() {
+	return coordinate.getOrdinat();
   }
 }
