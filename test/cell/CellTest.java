8d99dff071aa238bf41bca92112ef86b38c0651b
vinjerdim <marjer17dim@gmail.com>
2017-03-27 19:26:40 +0700

    hehe

---
+++ test/cell/CellTest.java
@@ -0,0 +1,22 @@
+/**
+ * Test untuk kelas Cell
+ */
+package cell;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import cell.Cell;
+
+/**
+ * @author Marvin Jerremy Budiman (13515076)
+ *
+ */
+public class CellTest {
+
+  @Test
+  public void test() {
+	Cell cell = new Cell(1,-5);
+	assertEquals(1,cell.getCellAbsis());
+	assertEquals(-5,cell.getCellAbsis());
+  }
+}
